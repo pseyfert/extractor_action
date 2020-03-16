a11e32f7897feac1616a9b2262f31923f9ca9ea9
Paul Seyfert <pseyfert.mathphys@gmail.com>
2020-03-16 22:04:42 +0100

    implement missing env vars

--- extractor_action_cmd/run.go
+++ extractor_action_cmd/run.go
@@ -15,15 +15,29 @@ func main() {
 		log.Printf("%v\n", err)
 		os.Exit(1)
 	}
+	envadds := strings.Split(os.Getenv("INPUT_ENV"), ":::")
+	additions := make(map[string]string)
+	for _, add := range envadds {
+		varsplit := strings.SplitN(add, "=", 2)
+		additions[varsplit[0]] = additions[varsplit[1]]
+	}
+
+	argrepl := strings.Split(os.Getenv("INPUT_REPLACE_ARGS"), ":::")
+	replacements := make(map[string]string)
+	for _, rep := range argrepl {
+		repsplit := strings.SplitN(rep, "=", 2)
+		replacements[repsplit[0]] = replacements[repsplit[1]]
+	}
+
 	executer := compile_commands_json_executer.Executer{
-		Appends:    strings.Split(os.Getenv("INPUT_APPEND_ARGS"), ":"),
-		Prepends:   strings.Split(os.Getenv("INPUT_PREPEND_ARGS"), ":"),
-		RemoveArgs: strings.Split(os.Getenv("INPUT_REMOVE_ARGS"), ":"),
-		Exe:        os.Getenv("INPUT_EXE"),
-		AcceptTU:   strings.Split(os.Getenv("INPUT_ACCEPT_TUS"), ":"),
-		RejectTU:   strings.Split(os.Getenv("INPUT_REJECT_TUS"), ":"),
-		// Env:         os.Getenv(""),
-		// Replace:     os.Getenv(""),
+		Appends:     strings.Split(os.Getenv("INPUT_APPEND_ARGS"), ":"),
+		Prepends:    strings.Split(os.Getenv("INPUT_PREPEND_ARGS"), ":"),
+		RemoveArgs:  strings.Split(os.Getenv("INPUT_REMOVE_ARGS"), ":"),
+		Exe:         os.Getenv("INPUT_EXE"),
+		AcceptTU:    strings.Split(os.Getenv("INPUT_ACCEPT_TUS"), ":"),
+		RejectTU:    strings.Split(os.Getenv("INPUT_REJECT_TUS"), ":"),
+		Env:         additions,
+		Replace:     replacements,
 		Concurrency: concurrency,
 		TraceFile:   os.Getenv("INPUT_TRACE_FILE"),
 	}
