9f9ee946e5ba42bd924baaebdeec6437277aee98
Paul Seyfert <pseyfert.mathphys@gmail.com>
2020-03-16 21:38:37 +0100

    initial dump

--- extractor_action_cmd/run.go
+++ extractor_action_cmd/run.go
@@ -1,14 +1,36 @@
 package main
 
 import (
-	"fmt"
+	"log"
 	"os"
+	"strconv"
 	"strings"
+
+	"github.com/pseyfert/compile_commands_json_executer/lib"
 )
 
 func main() {
-	for _, e := range os.Environ() {
-		pair := strings.SplitN(e, "=", 2)
-		fmt.Printf("%s:\t%s\n", pair[0], pair[1])
+	concurrency, err := strconv.Atoi(os.Getenv("INPUT_CONCURRENCY"))
+	if err != nil {
+		log.Printf("%v\n", err)
+		os.Exit(1)
+	}
+	executer := compile_commands_json_executer.Executer{
+		Appends:    strings.Split(os.Getenv("INPUT_APPEND_ARGS"), ":"),
+		Prepends:   strings.Split(os.Getenv("INPUT_PREPEND_ARGS"), ":"),
+		RemoveArgs: strings.Split(os.Getenv("INPUT_REMOVE_ARGS"), ":"),
+		Exe:        os.Getenv("INPUT_EXE"),
+		AcceptTU:   strings.Split(os.Getenv("INPUT_ACCEPT_TUS"), ":"),
+		RejectTU:   strings.Split(os.Getenv("INPUT_REJECT_TUS"), ":"),
+		// Env:         os.Getenv(""),
+		// Replace:     os.Getenv(""),
+		Concurrency: concurrency,
+		TraceFile:   os.Getenv("INPUT_TRACE_FILE"),
+	}
+
+	err = executer.Run(os.Getenv("INPUT_BUILD_PATH"))
+	if err != nil {
+		log.Printf("%v\n", err)
+		os.Exit(1)
 	}
 }
